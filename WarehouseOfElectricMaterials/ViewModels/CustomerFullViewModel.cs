907ca92e0fdf9cc52d166e43da40937c2bacb5e3
Muniek <ramadisu@gmail.com>
2012-04-24 00:00:56 +0200

    Dorzucony szkielet CustomerFullViewModel

---
+++ WarehouseOfElectricMaterials/ViewModels/CustomerFullViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using WarehouseElectric.Models;
+using WarehouseElectric.DataLayer;
+using System.Windows.Controls;
+
+namespace WarehouseElectric.ViewModels
+{
+    class CustomerFullViewModel:ViewModelBase
+    {
+            
+    }
+}
