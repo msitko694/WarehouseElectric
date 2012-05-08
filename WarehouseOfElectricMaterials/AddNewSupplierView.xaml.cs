4b65c8d6b870b0562633ba90ed559953ea7d5f75
Ewa Olesz <ewa.olesz@gmail.com>
2012-05-08 17:39:29 +0200

    Dodanie nowego okna AddNewSupplierView i ModelView

---
+++ WarehouseOfElectricMaterials/AddNewSupplierView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WarehouseElectric
+{
+    /// <summary>
+    /// Interaction logic for AddNewSupplierView.xaml
+    /// </summary>
+    public partial class AddNewSupplierView : Window
+    {
+        public AddNewSupplierView()
+        {
+            InitializeComponent();
+        }
+    }
+}
